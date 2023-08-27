845e595ff722430514db2106902e849bb758fe07
letiziagirardi <letizia.g00@gmail.com>
2023-08-27 18:44:49 +0200

    Old version removed

--- version1/articles_web_service_v1/src/main/java/com/webapp/repository/BarcodeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.webapp.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.webapp.entities.Barcode;
-
-public interface BarcodeRepository extends JpaRepository<Barcode, Integer>
-{
-	Barcode findByBarcode(String Barcode);
-}
--- version1/articles_web_service_v1/src/main/java/com/webapp/service/BarcodeService.java
+++
@@ -1,7 +0,0 @@
-package com.webapp.service;
-import com.webapp.entities.Barcode;
-
-public interface BarcodeService
-{
-	public Barcode SelByBarcode(String Barcode);
-}
